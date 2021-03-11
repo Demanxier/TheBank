3d04dc2a5213821100cfd49ecc63de25f0d44c51
Demanxier <demanxier.rodrigues@gmail.com>
2021-03-10 22:40:20 -0300

    gitignore file

---
+++ DIO.Bank/Enum/TipoConta.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DIO.Bank
+{ 
+    public enum TipoConta
+    {
+        PessoaFisica = 1,
+        PessoaJuridica = 2
+    }
+}
